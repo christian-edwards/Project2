8a079507295581bc8c66dc0f10fc4fcfdac94035
christian-edwards <christianallane@gmail.com>
2021-11-15 16:38:59 -0500

    loginsetup

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -14,9 +14,12 @@ object Main {
       .getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
 
-
-
-
+    spark.sql("CREATE DATABASE IF NOT EXISTS project2DB")
+    spark.sql("USE project2DB")
+    spark.sql("DROP TABLE IF EXISTS loginDB")
+    spark.sql("CREATE TABLE IF NOT EXISTS loginDB(username varchar(255) ,password varchar(255),role varchar(255)) row format delimited fields terminated by ','")
+    spark.sql("LOAD DATA LOCAL INPATH 'Inputs/loginCredentials.txt' INTO TABLE loginDB")
+    spark.sql("SELECT * FROM loginDB").show()
 
 
 
